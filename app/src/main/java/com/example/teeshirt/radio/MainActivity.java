95b54503d2d991a8b22f51d33769a7a256871a56
owuor91 <owuor91@gmail.com>
2015-05-02 21:20:04 +0300

    Radiobuttons working

--- app/src/main/java/com/example/teeshirt/radio/MainActivity.java
+++ app/src/main/java/com/example/teeshirt/radio/MainActivity.java
@@ -4,36 +4,38 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static RadioGroup radioGroup;
+    public static RadioButton radio;
+    public static Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onButtonClickListener();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+   public void onButtonClickListener(){
+       radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
+       button = (Button)findViewById(R.id.button);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               int selected = radioGroup.getCheckedRadioButtonId();
+               radio = (RadioButton)findViewById(selected);
+               Toast.makeText(MainActivity.this, radio.getText().toString(), Toast.LENGTH_SHORT).show();
+           }
+       });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+   }
 }
