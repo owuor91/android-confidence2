92b97bbbb48334411d4d87363b3a867305ee33df
owuor91 <owuor91@gmail.com>
2015-05-03 09:57:38 +0300

    Addition functionality complete

--- app/src/main/java/com/example/teeshirt/radio/Second.java
+++ app/src/main/java/com/example/teeshirt/radio/Second.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.AnalogClock;
 import android.widget.Button;
 import android.widget.DigitalClock;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Second extends ActionBarActivity {
@@ -15,11 +17,16 @@ public class Second extends ActionBarActivity {
     private static AnalogClock analog;
     private static DigitalClock digital;
 
+    private static Button addbtn;
+    private static EditText e1, e2;
+    private static TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         ButtonClickListener();
+        addbtnClickListener();
     }
 
 
@@ -39,6 +46,23 @@ public class Second extends ActionBarActivity {
                     analog.setVisibility(AnalogClock.VISIBLE);
                     digital.setVisibility(DigitalClock.GONE);
                 }
+            }
+        });
+    }
+
+    public void addbtnClickListener(){
+        addbtn = (Button)findViewById(R.id.addbtn);
+        e1 = (EditText)findViewById(R.id.e1);
+        e2 = (EditText)findViewById(R.id.e2);
+        tv = (TextView)findViewById(R.id.tv);
+
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(e1.getText().toString());
+                int num2 = Integer.parseInt(e2.getText().toString());
+                int sum = num1+num2;
+                tv.setText(Integer.toString(sum));
             }
         });
     }
