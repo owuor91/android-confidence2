691254eced4f9a42543551ddf0b2ec1d312fbbfc
owuor91 <owuor91@gmail.com>
2015-05-03 09:25:43 +0300

    Toggle clock visibility

--- app/src/main/java/com/example/teeshirt/radio/Second.java
+++ app/src/main/java/com/example/teeshirt/radio/Second.java
@@ -4,36 +4,42 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.Button;
+import android.widget.DigitalClock;
 
 
 public class Second extends ActionBarActivity {
+    private static Button button;
+    private static AnalogClock analog;
+    private static DigitalClock digital;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        ButtonClickListener();
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_second, menu);
-        return true;
-    }
+    public void ButtonClickListener(){
+        button = (Button)findViewById(R.id.button);
+        analog = (AnalogClock)findViewById(R.id.analog);
+        digital = (DigitalClock)findViewById(R.id.digital);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (digital.getVisibility()== DigitalClock.GONE){
+                    digital.setVisibility(DigitalClock.VISIBLE);
+                    analog.setVisibility(AnalogClock.GONE);
+                }
+                else {
+                    analog.setVisibility(AnalogClock.VISIBLE);
+                    digital.setVisibility(DigitalClock.GONE);
+                }
+            }
+        });
     }
 }
